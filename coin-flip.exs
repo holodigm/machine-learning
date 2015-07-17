de6135c5f8f1b5f301e06e4c9c8072cb8a1e19b0
Kym McInerney <kym@holodigm.com.au>
2015-07-17 13:58:34 +1000

    clean up Ruby iterations

---
+++ coin-flip.exs
@@ -0,0 +1,56 @@
+# Hoeffding Inequality
+
+# Run a computer simulation for flipping 1,000 virtual fair coins.
+# Flip each coin independently 10 times.
+# Focus on 3 coins as follows:
+#   c1 is the first coin flipped,
+#   crand is a coin chosen randomly from the 1,000, and
+#   cmin is the coin which had the minimum frequency of heads (pick the earlier one in case of a tie).
+
+#   Let ν1, νrand, and νmin be the fraction of heads obtained for the 3 respective coins out of the 10 tosses.
+
+# Run the experiment 100,000 times in order to get a full distribution
+# of ν1, νrand, and νmin (note that crand and cmin will change from run to run).
+
+
+defmodule CoinFlip do
+  1..100000 |> Enum.map fn(x) -> x |> IO.puts end
+
+end
+  n = 100_000
+
+  v_first = 0
+  v_min = 0
+  v_rand = 0
+  first = 0
+  rand = 0
+
+  (1..n).each do
+    rand_coin = Random.rand(1000)
+    min = 1.0
+
+    (1..1000).each do |coin|
+    i = 0
+    (1..10).each do |flip|
+    if rand.round == 0
+    flip = 1
+  else
+    flip = 0
+  end
+  i += flip
+end
+first = i / 10 if coin == 1
+rand = i / 10 if coin == rand_coin
+min = i / 10 if (i / 10) < min
+end
+v_first += first
+v_rand += rand
+v_min += min
+print '.'
+  # p v_min / iter
+end
+
+p "vfirst: #{v_first / n}"
+p "vrand: #{v_rand / n}"
+p "vmin: #{v_min / n}"
+end
--- coin-flip.rb
+++ coin-flip.rb
@@ -12,7 +12,9 @@
 # Run the experiment 100,000 times in order to get a full distribution
 # of ν1, νrand, and νmin (note that crand and cmin will change from run to run).
 
-n = 100_000
+iterations = 1000
+coins = 1000
+flips = 10
 
 v_first = 0
 v_min = 0
@@ -20,31 +22,32 @@ v_rand = 0
 first = 0
 rand = 0
 
-(1..n).each do
-  rand_coin = Random.rand(1000)
-  min = 1.0
-
-  (1..1000).each do |coin|
-    i = 0
-    (1..10).each do |flip|
-      if rand.round == 0
-        flip = 1
+(1..iterations).each do
+  min = 1
+  rand_coin = Random.rand(coins)
+  (1..coins).each do |coin|
+    call = 0.0
+    (1..flips).each do
+      r = Random.rand.round
+      if r == 0
+        heads = 1
       else
-        flip = 0
+        heads = 0
       end
-      i += flip
+      call += heads
     end
-    first = i / 10 if coin == 1
-    rand = i / 10 if coin == rand_coin
-    min = i / 10 if (i / 10) < min
+    first = (call / flips) if coin == 1
+    if coin == rand_coin
+      rand = (call / flips)
+      #p "Random Coin #{rand_coin} was Heads #{call} times or #{rand}"
+    end
+    min = (call / flips) if (call / flips) < min
   end
   v_first += first
   v_rand += rand
   v_min += min
-  print '.'
-  # p v_min / iter
 end
 
-p "vfirst: #{v_first / n}"
-p "vrand: #{v_rand / n}"
-p "vmin: #{v_min / n}"
+p "vfirst: #{v_first / iterations}"
+p "vrand: #{v_rand / iterations}"
+p "vmin: #{v_min / iterations}"
