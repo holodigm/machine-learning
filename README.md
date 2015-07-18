afb33001cafaa35202b05a82caf8b853d664a76e
Kym McInerney <kym@holodigm.com.au>
2015-07-18 18:46:51 +1000

    Update README and syntax

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # machine-learning
+
+
+## Hoeffding Inequality
+
+Run a computer simulation for flipping 1,000 virtual fair coins.
+Flip each coin independently 10 times.
+Focus on 3 coins as follows:
+**  c1 is the first coin flipped,
+**  crand is a coin chosen randomly from the 1,000, and
+**  cmin is the coin which had the minimum frequency of heads (pick the earlier one in case of a tie).
+
+* Let ν1, νrand, and νmin be the fraction of heads obtained for the 3 respective coins out of the 10 tosses.
+
+Run the experiment 100,000 times in order to get a full distribution of ν1, νrand, and νmin (note that crand and cmin will change from run to run).
--- coin-flip.exs
+++ coin-flip.exs
@@ -1,17 +1,5 @@
 # Hoeffding Inequality
 
-# Run a computer simulation for flipping 1,000 virtual fair coins.
-# Flip each coin independently 10 times.
-# Focus on 3 coins as follows:
-#   c1 is the first coin flipped,
-#   crand is a coin chosen randomly from the 1,000, and
-#   cmin is the coin which had the minimum frequency of heads (pick the earlier one in case of a tie).
-
-#   Let ν1, νrand, and νmin be the fraction of heads obtained for the 3 respective coins out of the 10 tosses.
-
-# Run the experiment 100,000 times in order to get a full distribution
-# of ν1, νrand, and νmin (note that crand and cmin will change from run to run).
-
 defmodule Coin do
   def iterations do
     1000
@@ -42,7 +30,6 @@ defmodule Coin do
   def iterate do
     Enum.map(1..iterations, fn (iteration) ->
       rand_coin = :random.uniform(coins)
-      IO.puts "."
       flip(coins, rand_coin)
     end)
   end
